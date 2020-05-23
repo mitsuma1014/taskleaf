411b09b043a078408a2327718b6074f4178dfcc3
mitsuma1014 <65802385+mitsuma1014@users.noreply.github.com>
2020-05-23 14:36:32 +0900

    空行削除

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,7 +10,6 @@ class TasksController < ApplicationController
   
   def index
     @q = current_user.tasks.ransack(params[:q])
-   
     @tasks = @q.result(distinct: true).page(params[:page])
 
     respond_to do |format|
